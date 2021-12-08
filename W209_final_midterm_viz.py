c86be1ef0326e8319f1ed9c37808ed635bc3121d
Mickey Piekarski <mickeypiekarski@Mickeys-MacBook-Pro-2.local>
2021-12-08 15:16:18 -0500

    done and pretty

--- W209_final_midterm_viz.py
+++ W209_final_midterm_viz.py
@@ -9,24 +9,32 @@ from datetime import datetime
 import numpy as np
 import altair as alt
 
+from fbprophet import Prophet
+import matplotlib.pyplot as plt
+
 
 key = "266dbf914fd18fad344fcf6e0937362695777573"
 nomics = Nomics(key)
 
-# **Load Data**
 
-#markets = nomics.ExchangeRates.get_history(currency = 'BTC', start = '2015-10-02T15:00:00.05Z')
-#markets = pd.DataFrame(markets)
-#print(markets)
-#markets.to_csv('sample_df.csv')
+
+"""
+Functions included:
+
+ - load_data: returns data in a standardized format
+ - candlestick_chart: creates our candlestick chart
+ - get_mape: i have no clue tbh
+ - create_forecasting_chart: supposed to create  (you gessued it) a forecasting chart.
+    currently doesn't work. not deployed.
+ - volatility_chart: creates out volatility chart
+ - percent_change: creates our percent change chart
+ - rolling_avg_std: returns two charts. rolling average and stdev against time
+ - reddit_posts_and_price: returns the reddit viz
+ """
+
+
+
 def load_data(df):
-
-    #load data in as csv
-    #try loading in that way
-    #similar to how mickey did it
-
-
-
 
     #gets the 10 day real volatlilty variance swap and classical ways, and 10-day STD
     def getvol(df, window):
@@ -58,129 +66,18 @@ def load_data(df):
     df = df.fillna(method="pad")
     df = df.fillna(method="bfill")
 
-
     return df
 
 
-
-#issue with dict columns
-#get from_dict columns
-#add to date column to existing df
-# join from_dict columns with exising df on date column
-    # dicts = ['1d', '7d', '30d', '365d', 'ytd']
-    # for i in dicts:
-    #     new_df = df.join(pd.DataFrame.from_dict(df[i]))
-    #     new_df['Date'] = df['Date']
-    #     new_df['id'] = df['id']
-    #     df.merge(new_df, on=['Date', 'id'])
-
-
-#sample_df = load_data(markets)
-#sample_df = pd.read_csv('sample_df.csv')
-#print(sample_df.head())
-
-
-
-"""
-tokens = ['ADA ETH ,BTC, XRP, TRX, DOGE, LTC, BCH, VNQ, SPY']
-time.sleep(1)
-currencies  = nomics.Currencies.get_currencies(ids = 'ADA, ETH ,BTC, XRP, TRX, DOGE, LTC, BCH, VNQ, SPY')
-currencies = pd.DataFrame(currencies)
-currencies.head()
-
-
-#nomics = load_data('sample_df.csv', 'btc')
-#nomics.head()
-
-# Time Series Forecasting
-
-
-# **Notes**
-#
-# - Need to create one selector for all visualizations, figure out how to include multiple selectors in visualizations
-# consider using facet to break out performance if layered/stacked charts become too common.
-# - Having issue with scatterplot, once it's finished it should work better.
-# - implement mean line for some value metric [link](https://altair-viz.github.io/gallery/selection_layer_bar_month.html)
-# - implement comparison to other assets (Real estate, S&P etc)
-#     - include volatility, price, and other metrics
-# - need to make correlation matrix [link](https://github.com/altair-viz/altair/pull/1945)
-# - inspiration [link](https://www.youtube.com/watch?v=XWdcpoXKRnk)
-# - Visualize the number of comments vs crypto with overlay of price (size=comments)
-# - add sharpe ratio, volatility (standard dev), speed (volume traded)
-
-
-# **Ignore the Following Cells:**
-
-
-#Creates chart with selected currency and returns from SPY in percent change
-def create_comparison_with_others(df, other_df):
-
-# input_dropdown1 = alt.binding_select(options=[None] + list(final_union_df['token'].unique()), labels=label_list)
-# token_selector1 = alt.selection_single(name='Currency: ', fields=['token'], bind=input_dropdown1)
-# #token_selector2 = alt.selection_single(name='x_axis', fields=['token'], bind=input_dropdown)
-# brush = alt.selection(type='interval')
-    brush2 = alt.selection_multi(fields=['token'])
-
-
-#scales = alt.selection_interval(bind='scales')
-
-# #select time period with this small chart
-# small = alt.Chart(final_union_df).mark_bar(tooltip=True).encode(
-#     x=alt.X('Date:T', title='Date', axis=alt.Axis(format='%m/%d', labelAngle=-45), )
-#     #,y=alt.Y('Date:T', axis=alt.Axis(format='%m/%y', labelAngle=-45))
-#     ,color=alt.condition(brush, alt.value('red'), alt.value('grey'))
-#     ).add_selection(brush).properties(width=875, height=100)
-
-# #select multiple tokens with this small chart
-    others = alt.Chart(other_df).mark_bar(tooltip=True).encode(
-        x=alt.X('token:O', title=None, axis=alt.Axis(labelAngle=-45))
-        ,color=alt.condition(brush2, alt.value('blue'), alt.value('grey'))
-        ).add_selection(brush2).properties(width=800, height=100)
-
-
-#bar height is max price for each token?
-
-    large = alt.Chart(df).mark_line(tooltip=True).encode(
-        x=alt.X('Date:T', title='Date', axis=alt.Axis(format='%m/%d/%y', labelAngle=-45))
-        ,y=alt.Y('percent_change:Q', title='Percent Change', axis=alt.Axis(format='%',  labels=True, tickSize=0))
-        ,color='token'
-        #alt.condition(brush, alt.value('red'), alt.value('grey'))
-        ).transform_filter(brush2).properties(width=800, height=500).interactive()
-# .add_selection(
-#          token_selector1
-#          ).transform_filter(
-#              token_selector1).transform_filter(brush)
-
-
-
-
-    both = alt.vconcat(others, large)
-    #both = alt.vconcat(both, large)
-    both.configure_legend(
-    strokeColor='gray',
-    fillColor='#EEEEEE',
-    padding=10,
-    cornerRadius=10,
-    orient='top-right'
-    )
-    return both
-
-#create_comparison_with_others()
-
-# **Begin at this cell**
-
-
-# **Needs to be fixed Chart doesn't display, might be issue with date format**
-
-"""
 def candlestick_chart(df):
+    """
 
+    Creates the Candle stick chart
+
+    """
     open_close_color = alt.condition("datum.open <= datum.close",
                                     alt.value("#06982d"),
                                     alt.value("#ae1325"))
-
-    #scales = alt.selection_interval(bind='scales')
-
 
     base = alt.Chart(df).encode(
             alt.X('Date:T',
@@ -193,7 +90,6 @@ def candlestick_chart(df):
             tooltip=[alt.Tooltip('Date:T'), alt.Tooltip('open:Q', format='$'),
                     alt.Tooltip('close:Q', format='$')]
         )
-        #
 
     rule = base.mark_rule().encode(
             alt.Y(
@@ -213,17 +109,12 @@ def candlestick_chart(df):
     return all.properties(title='Price of Currency over Time', width=1500, height=400).interactive()
 
 
-
-
 # Prophet Time series forcasting
 
 
 def get_mape(actuals, forecasts):
     MAPE = np.mean(np.abs(forecasts - actuals) / (actuals))
     return MAPE
-
-from fbprophet import Prophet
-import matplotlib.pyplot as plt
 
 
 def create_forecasting_chart(df):
@@ -279,84 +170,7 @@ def create_forecasting_chart(df):
     all = bar + area + line
     return all.properties(width=800, height=600).interactive()
 
-"""
-crypto_prophet = Prophet()
 
-experiment_df = pd.read_csv('sample_df.csv')
-experiment_df['ds'] = pd.to_datetime(experiment_df['timestamp']).dt.date
-experiment_df = experiment_df.rename(columns={'rate': 'y'})
-experiment_df = experiment_df.drop(columns={'Unnamed: 0', 'timestamp'})
-train_data = experiment_df[pd.to_datetime(experiment_df['ds']) <= '2021-10-01']
-valid_data = experiment_df[pd.to_datetime(experiment_df['ds']) > '2021-10-01']
-
-# Fit the model on the time series.
-m_prophet = crypto_prophet.fit(train_data)
-
-# Create a DataFrame of future dates to create forecasts for.
-future_prophet = crypto_prophet.make_future_dataframe(periods = 30)
-
-# Create forecast
-prophet_forecast = crypto_prophet.predict(future_prophet)
-
-
-m_prophet.plot(prophet_forecast)
-prophet_forecast.tail()
-
-
-print('MAPE: ', get_mape(experiment_df['y'], prophet_forecast['yhat']))
-
-prophet_forecast['y'] = experiment_df['y']
-prophet_forecast['low'] = prophet_forecast.loc[:,'yhat':'y'].min(axis=1)
-prophet_forecast['high'] = prophet_forecast.loc[:,'yhat':'y'].max(axis=1)
-prophet_forecast['error'] = prophet_forecast['yhat_upper'] - prophet_forecast['yhat_lower']
-prophet_forecast['midpoint'] = prophet_forecast['low'] + abs((prophet_forecast['y'] - prophet_forecast['yhat']) / 2)
-prophet_forecast.head()
-
-def create_forecasting_chart(prophet_forecast):
-
-  difference = alt.condition("datum.yhat > datum.y",
-                                      alt.value("#06982d"),
-                                      alt.value("#ae1325"))
-
-  diff2= alt.condition("datum.yhat > datum.y",
-                                      alt.value("#06982d"),
-                                      alt.value("#ae1325"))
-
-  base = alt.Chart(prophet_forecast).encode(
-      x=alt.X('ds:T', title='Date', axis=alt.Axis(format='%m/%d/%y', labelAngle=-45))
-
-      )
-
-
-  bar = base.mark_bar().encode(
-      alt.Y('y:Q'),
-      alt.Y2('yhat:Q'), color = difference)
-  area = base.mark_area(opacity = 0.2, color='steelblue').encode(
-      alt.Y('yhat_upper:Q'),
-      alt.Y2('yhat_lower:Q'))
-
-  line = base.mark_line(color='black').encode(
-      alt.Y('midpoint:Q')
-  )
-
-
-  all = bar + area + line
-  all.properties(width=800, height=600).interactive()
-
-
-# **Volatlilty Chart**
-#
-# **Unable to display area chart with price**
-
-
-sample_df.head()
-
-#Creates chart with volatlilty overlayed with price,
-#takes in dataframe with price and vol data, and
-#type- string specifying the columns name with :Q
-
-
-"""
 def volatlilty_chart(df, vol):
     vol = alt.Chart(df).mark_line(tooltip=True).encode(
             x=alt.X('Date:T', title='Date', axis=alt.Axis( grid = False)),
@@ -370,152 +184,8 @@ def volatlilty_chart(df, vol):
     return total.properties(width=750, height=500).interactive()
 
 
-#  #select multiple tokens with this small chart
-# others = alt.Chart(nomics).mark_bar(tooltip=True).encode(
-#         x=alt.X('token:O', title=None, axis=alt.Axis(labelAngle=-45))
-#         ,color=alt.condition(brush2, alt.value('blue'), alt.value('grey'))
-#         ).add_selection(brush2).properties(width=800, height=100)
 
 
-# #bar height is max price for each token?
-
-# large = alt.Chart(nomics).mark_line(tooltip=True).encode(
-#         x=alt.X('Date:T', title='Date', axis=alt.Axis(format='%m/%d/%y', labelAngle=-45))
-#         ,y=alt.Y('10-day STD:Q', title='10 Day Volalilty', axis=alt.Axis(labels=True, tickSize=0))
-#         ,color='token'
-#         ).transform_filter(brush2).properties(width=800, height=500).interactive()
-
-
-
-    # both = alt.vconcat(others, large)
-    # #both = alt.vconcat(both, large)
-    # both.configure_legend(
-    # strokeColor='gray',
-    # fillColor='#EEEEEE',
-    # padding=10,
-    # cornerRadius=10,
-    # orient='top-right'
-    # )
-    # return both
-
-
-#viz = volatlilty_chart(sample_df, '10-day STD:Q')
-
-
-# **Needs to be fixed Chart doesn't display, might be issue with date format**
-
-
-# FB prophet TS forecasting
-"""
-
-from fbprophet import Prophet
-import matplotlib.pyplot as plt
-
-crypto_prophet = Prophet()
-
-experiment_df = pd.read_csv('sample_df.csv')
-experiment_df['ds'] = pd.to_datetime(experiment_df['timestamp']).dt.date
-experiment_df = experiment_df.rename(columns={'rate': 'y'})
-experiment_df = experiment_df.drop(columns={'Unnamed: 0', 'timestamp'})
-train_data = experiment_df[pd.to_datetime(experiment_df['ds']) <= '2021-10-01']
-valid_data = experiment_df[pd.to_datetime(experiment_df['ds']) > '2021-10-01']
-
-# Fit the model on the time series.
-m_prophet = crypto_prophet.fit(train_data)
-
-# Create a DataFrame of future dates to create forecasts for.
-future_prophet = crypto_prophet.make_future_dataframe(periods = 30)
-
-# Create forecast
-prophet_forecast = crypto_prophet.predict(future_prophet)
-
-
-m_prophet.plot(prophet_forecast)
-prophet_forecast.tail()
-
-
-print('MAPE: ', get_mape(experiment_df['y'], prophet_forecast['yhat']))
-
-# def create_reddit_ds(df, token):
-#     df = df.rename(columns={'created_utc': 'Date'})
-#     df[token] = token
-#     df = df.merge(final_union_df, how='left', on='Date')
-
-# reddit_posts = pd.read_csv('reddit_posts_btc.csv')
-# #reddit_posts = create_reddit_ds(reddit_posts, 'btc')
-# series = str(reddit_posts['created_utc']).split()[]
-
-sample_df.head()
-
-#aggregate post by day
-#add to final_union_df
-
-# Get rid of non dates in date column
-
-
-reddit = pd.read_csv('reddit_posts_btc.csv')
-idx = reddit.index[reddit['created_utc'] == 'لباس کار']
-reddit = reddit.drop(idx)
-idx = reddit.index[reddit['created_utc'] == 'Need some help to understand what the issue is with F1 24TH Cheetah miners']
-reddit = reddit.drop(idx)
-reddit['created_utc'] = pd.to_datetime(reddit['created_utc'])
-reddit['date'] = reddit['created_utc'].dt.date
-reddit['hour'] = reddit['created_utc'].dt.hour
-reddit_posts_granular = reddit.groupby(['date', 'hour']).agg({'title': 'count'})
-reddit_posts_granular = reddit_posts_granular.reset_index()
-#reddit_posts_hour = reddit.groupby(['hour']).agg({'title': 'count'})
-reddit_posts_granular['prev_hour'] = reddit_posts_granular['title'].shift(1)
-reddit_posts_granular['percent_change'] = (reddit_posts_granular['title'] - reddit_posts_granular['prev_hour']) / reddit_posts_granular['prev_hour']
-reddit_posts_granular.head()
-
-
-# **Reddit Posts Chart**
-
-
-# # **Noor's Visualizations**
-
-
-# Merged w/ Marcus pre-processing stuff:
-# ada_df = pd.read_csv('ADA-USD.csv')
-# eth_df = pd.read_csv('ETH-USD.csv')
-# btc_df = pd.read_csv('BTC-USD.csv')
-# xrp_df = pd.read_csv('XRP-USD.csv')
-# trx_df = pd.read_csv('TRX-USD.csv')
-# doge_df = pd.read_csv('DOGE-USD.csv')
-# ltc_df = pd.read_csv('LTC-USD.csv')
-# bch_df = pd.read_csv('BCH-USD.csv')
-
-
-# ada_df['token'] = 'ADA'
-# eth_df['token'] = 'ETH'
-# btc_df['token'] = 'BTC'
-# xrp_df['token'] = 'XRP'
-# trx_df['token'] = 'TRX'
-# doge_df['token'] = 'DOGE'
-# ltc_df['token'] = 'LTC'
-# bch_df['token'] = 'BCH'
-
-# def pre_processing(df):
-#     df['Change'] = df['Open'] - df['Adj Close']
-#     df['Max Close'] = df['Close'].max()
-#     df['Mean Close'] = df['Close'].mean()
-#     return df
-
-# df_list = [ada_df, eth_df, btc_df, xrp_df, trx_df, doge_df, ltc_df, bch_df]
-# for df in df_list:
-#     pre_processing(df)
-
-# def concatenator(df_list):
-#     union_df = pd.concat(df_list)
-#     union_df['percent_change'] = (union_df['Adj Close'] - union_df['Open']) / union_df['Adj Close']
-#     return union_df
-
-# union_df = concatenator(df_list)
-# union_df.head()
-
-# Percentage change chart:
-
-"""
 def percent_change(df):
     chart = alt.Chart(df).mark_bar().encode(
         alt.X("Date:T"),
@@ -568,64 +238,6 @@ def rolling_avg_std(df):
 
     return (chart1 + moving_avg).interactive(), (chart2 + moving_std).interactive()
 
-"""
-rolling_avg_std(sample_df)
-
-# btc_reddit_data = pd.read_csv('reddit_posts_btc.csv')
-# btc_reddit_data['mentioned?'] = btc_reddit_data.selftext.str.contains('BTC', case=True, flags=0) | btc_reddit_data.title.str.contains('BTC', case=True, flags=0)| btc_reddit_data.title.str.contains('Bitcoin', case=True, flags=0)| btc_reddit_data.selftext.str.contains('Bitcoin', case=True, flags=0)| btc_reddit_data.title.str.contains('bitcoin', case=True, flags=0)| btc_reddit_data.selftext.str.contains('bitcoin', case=True, flags=0)
-# btc_reddit_data['mentioned?'] = btc_reddit_data['mentioned?'].astype(int)
-# for item in btc_reddit_data['created_utc']:
-#     btc_reddit_data['Date'] = datetime.strptime(item, '%m/%d/%Y %H:%M')
-# # btc_reddit_data['Date'].apply(lambda x: x.strftime('%Y-%m'))
-# btc_reddit_data = btc_reddit_data[: 5000]
-
-sample_df.head()
-
-def load_reddit_data(reddit):
-
-    idx = reddit.index[reddit['created_utc'] == 'لباس کار']
-    reddit = reddit.drop(idx)
-    idx = reddit.index[reddit['created_utc'] == 'Need some help to understand what the issue is with F1 24TH Cheetah miners']
-    reddit = reddit.drop(idx)
-    reddit['created_utc'] = pd.to_datetime(reddit['created_utc'])
-    reddit['date'] = reddit['created_utc'].dt.date
-    reddit['hour'] = reddit['created_utc'].dt.hour
-    reddit_posts_granular = reddit.groupby(['date']).agg({'title': 'count'})
-    reddit_posts_granular = reddit_posts_granular.reset_index()
-    #reddit_posts_hour = reddit.groupby(['date']).agg({'title': 'count'})
-    reddit_posts_granular['prev_date'] = reddit_posts_granular['title'].shift(1)
-    reddit_posts_granular['percent_change'] = (reddit_posts_granular['title'] - reddit_posts_granular['prev_date']) / reddit_posts_granular['prev_date']
-    reddit_posts_granular['date'] = pd.to_datetime(reddit_posts_granular['date'])
-    return reddit_posts_granular
-
-reddit_posts_granular = load_reddit_data(pd.read_csv('reddit_posts_btc.csv'))
-
-# Reddit Number of Posts and Currency Price Over Time:
-# Create x-axis (time series):
-# Changed label rotation for x-axis for visual ease:
-
-def reddit_posts_and_price(df1, df2):
-    base1 = alt.Chart(df1).encode(x=alt.X('Date:T', axis=alt.Axis(labelAngle=45)))
-
-    bar = base1.mark_bar(color = 'lightblue').encode(
-        alt.Y('close:Q'))
-
-    base2 = alt.Chart(df2).encode(x=alt.X('date:T', axis=alt.Axis(labelAngle=45)))
-
-    line = base2.mark_line(color = 'red').encode(
-        alt.Y('title:Q'))
-
-    chart = alt.layer(bar, line).resolve_scale(y='independent').encode(
-            tooltip=['Date:T', 'Close:Q']
-        )
-
-    return chart.properties(width=3000, title='Currency Price in Relation to Number of Reddit Posts Over Time').interactive()
-
-
-reddit_posts_and_price(sample_df, reddit_posts_granular)
-"""
-
-
 
 
 def reddit_posts_and_price(price, reddit, token):
@@ -653,4 +265,4 @@ def reddit_posts_and_price(price, reddit, token):
             tooltip=['Date:T', 'close:Q']
         )
 
-    return chart.properties(width=3000, title='Currency Price in Relation to Number of Reddit Posts Over Time').interactive()
+    return chart.properties(width=3000, title='Currency Price in Relation to Number of Reddit Posts Over Time. NOTE: Only YTD Data').interactive()
--- final_streamlit.py
+++ final_streamlit.py
@@ -15,107 +15,158 @@ import matplotlib.pyplot as plt
 from W209_final_midterm_viz import *
 
 
-
-
-time.sleep(1)
 #configure page_title
 
-currencies_name = ['BTC', 'ETH', 'BNB', 'USDT', 'SOL', 'ADA', 'XRP', 'USDC', 'DOT', 'DOGE']
-st.set_page_config(
-     page_title='Streamlit cheat sheet',
-     layout="wide",
-     initial_sidebar_state="expanded",
-)
+
+def startup():
+
+    #configure page
+    st.set_page_config(
+         page_title='Crypto Dashboard',
+         layout="wide",
+         initial_sidebar_state="expanded",
+    )
+
+    #title and subheader
+    st.title("Crypto Dashboard")
+    st.subheader("Mickey Piekarski, Varun Dashora, Noor Gill, Marcus Manos")
+
+    #currencies our app supports
+    global currencies_name
+    currencies_name = ['BTC', 'ETH', 'BNB', 'USDT', 'SOL','AVAX', 'USDC', ]
 
 
-img = Image.open("cryptoreview_logo.jpg")
-st.title("Crypto Dashboard")
-st.subheader("Mickey Piekarski, Varun Dashora, Noor Gill, Marcus Manos")
+def sidebar():
+    #creating the sidebar
 
-st.sidebar.image(img, width = 100)
-st.sidebar.write("Send us some [feedback](https://docs.google.com/forms/d/e/1FAIpQLSeW1-wPirsWOBxF8VSJUxIGd1bM9BnT55cX5EXK6atmzAO3Hw/viewform?usp=sf_link)!")
+    img = Image.open("cryptoreview_logo.jpg")
+    st.sidebar.image(img, width = 100)
+    st.sidebar.write("Send us some [feedback](https://docs.google.com/forms/d/e/1FAIpQLSeW1-wPirsWOBxF8VSJUxIGd1bM9BnT55cX5EXK6atmzAO3Hw/viewform?usp=sf_link)!")
 
-global option
-option = st.sidebar.selectbox(\
-'Which coin would you like to view', currencies_name)
-st.sidebar.write('You selected: ', option)
+    #choose coin option
+    global coin
+    coin = st.sidebar.selectbox(\
+    'Which coin would you like to view', currencies_name)
+    st.sidebar.write('You selected: ', coin)
 
-global option1
+    #choose timerame
+    global option1
+    option1 = st.sidebar.selectbox(\
+    'Date range (years)', ['YTD',0.5,1,2,3,4,5,])
+    st.sidebar.write('You selected: ', option1)
 
-option1 = st.sidebar.selectbox(\
-'Date range (years)', [0.5,1,2,3,4,5])
-st.sidebar.write('You selected: ', option1)
+    ## goal, intended audience, data sources
+    st.sidebar.markdown("""
+    **Goal:** Our goal is to help users understand cryptocurrency trends relating to price and media/Reddit sentiment through visualizations in order to assist in decision-making for investment portfolios.
+    **Intended Audience:** The intended audience is both professional and amateur cryptocurrency investors who wish to use these metrics and data to refine their portfolios. A secondary audience would be financial or economic researchers who wish to use this information to assist their analysis of the market.
+    **Data sources:** Cryptoreview.ai and Nomics.api
+    """)
 
 
-st.sidebar.markdown("""
-**Goal:** Our goal is to help users understand cryptocurrency trends relating to price and media/Reddit sentiment through visualizations in order to assist in decision-making for investment portfolios.
-
-**Intended Audience:** The intended audience is both professional and amateur cryptocurrency investors who wish to use these metrics and data to refine their portfolios. A secondary audience would be financial or economic researchers who wish to use this information to assist their analysis of the market.
-
-**Data sources:** Cryptoreview.ai and Nomics.api
-""")
 #start up api
-key = "266dbf914fd18fad344fcf6e0937362695777573"
-nomics = Nomics(key)
+def startup_api_load_data():
+
+    """
+    df: price history of coin chosen
+    reddit: reddit posts information
+    currencies: metadata of coin ( volume, price, img etc)
+    """
+    global df
+    global reddit
+    global currencies
+    global ytd
+    # nomics api call
+    key = "266dbf914fd18fad344fcf6e0937362695777573"
+    nomics = Nomics(key)
+    markets = nomics.ExchangeRates.get_history(currency = coin, start = '2015-10-02T15:00:00.05Z')
+
+    #NOTE: we must sleep for 1 sewcond after each API call bc we are only allowd one call per second
+
+    time.sleep(1)
+
+    df = pd.DataFrame(markets)
+
+    #will need to change on new year
+    ytd = df[df['timestamp']>'2021']
+    if option1 == 'YTD':
+        df = ytd.copy()
+    else:
+        df = df.tail(int(364 * option1))
 
 
-coin = option
-markets = nomics.ExchangeRates.get_history(currency = coin, start = '2015-10-02T15:00:00.05Z')
-time.sleep(1)
-df = pd.DataFrame(markets)
-df = df.tail(int(364 * option1))
+    #hackey solution to final reddti viz
 
-#df.to_csv('sample_df.csv')
-#df = pd.read_csv('sample_df.csv')
-df = load_data(df)
-reddit = pd.read_csv('data_for_viz_project_dec052021.csv')
-#df
-
-
-
-
-col1, col2,col3, col4 = st.columns(4)
-
-
-
-
-
-
-with col1:
+    df = load_data(df)
+    ytd = load_data(ytd)
+    reddit = pd.read_csv('data_for_viz_project_dec052021.csv')
     currencies  = nomics.Currencies.get_currencies(coin)
     currencies = pd.DataFrame(currencies)
-    val1 = currencies['price'][0]
-    val2 = currencies['1d'][0]['price_change']
-    logo = np.array(currencies['logo_url'])[0]
-    st.image(logo,width = 100)
 
 
 
 
-with col2:
-    st.metric("Price",smart_num(val1), smart_num(val2))
-    val1 = currencies['1d'][0]['volume']
-    val2 = currencies['1d'][0]['volume_change']
-    val3 = currencies['market_cap'][0]
-    val4 = currencies['1d'][0]['market_cap_change']
 
-with col3:
-    st.metric("Volume 1d",smart_num(val1),smart_num(val2))
+def coin_metadata():
 
-with col4:
+    col1, col2,col3, col4 = st.columns(4)
+
+    #display coin logo
+    with col1:
+        logo = np.array(currencies['logo_url'])[0]
+        st.image(logo,width = 100)
+
+    #display price (with change)
+    with col2:
+        val1 = currencies['price'][0]
+        val2 = currencies['1d'][0]['price_change']
+        st.metric("Price",smart_num(val1), smart_num(val2))
+
+    #display volume (with change)
+    with col3:
+        val1 = currencies['1d'][0]['volume']
+        val2 = currencies['1d'][0]['volume_change']
+        st.metric("Volume 1d",smart_num(val1),smart_num(val2))
+
+    #display market cap (with change)
+    with col4:
+        val3 = currencies['market_cap'][0]
+        val4 = currencies['1d'][0]['market_cap_change']
         st.metric("Market Cap", smart_num(val3),smart_num(val4))
 
-st.altair_chart(candlestick_chart(df),use_container_width = True)
 
-col1b, col2b = st.columns(2)
-ra1,ra2 = rolling_avg_std(df)
-with col1b:
 
-    st.altair_chart(volatlilty_chart(df,'10-day STD:Q'),use_container_width = True)
-    st.altair_chart(ra1,use_container_width = True)
 
-    z = reddit_posts_and_price(df,reddit,coin)
-    st.altair_chart(z, use_container_width = True)
-with col2b:
-    st.altair_chart(percent_change(df),use_container_width = True)
-    st.altair_chart(ra2,use_container_width = True)
+def VIZ_TIME_BABY():
+
+    #creating the charts
+    candle = candlestick_chart(df)
+    volatility = volatlilty_chart(df,'10-day STD:Q')
+    ra1,ra2 = rolling_avg_std(df)
+    reddit_chart = reddit_posts_and_price(ytd,reddit,coin)
+    percent = percent_change(df)
+
+
+    #wide candlestick_chart
+    st.altair_chart(candle,use_container_width = True)
+
+    col1b, col2b = st.columns(2)
+
+    # first column volatility rolling average 1 and reddit
+    with col1b:
+        st.altair_chart(volatility,use_container_width = True)
+        st.altair_chart(ra1,use_container_width = True)
+
+
+    #colums 2 percent change and rolling average 2 chart
+    with col2b:
+        st.altair_chart(percent,use_container_width = True)
+        st.altair_chart(ra2,use_container_width = True)
+    st.altair_chart(reddit_chart, use_container_width = True)
+
+
+if __name__ == "__main__":
+    startup()
+    sidebar()
+    startup_api_load_data()
+    coin_metadata()
+    VIZ_TIME_BABY()
